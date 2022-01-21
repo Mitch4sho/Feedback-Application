1b4348bedf9f97219a71b26040bd5f21712cc2db
Mitchell Oliver <Mitchelloliver1991@gmail.com>
2022-01-20 21:56:07 -0800

    added styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
-
+import Header from "./components/Header";
 export default function App() {
   return (
-    <div>
+    <div className="container">
+      <Header />
       <h1>Hello world</h1>
     </div>
   );
