10c0aebc4e14607997e7463eefd261feb5fef34c
Mitchell Oliver <Mitchelloliver1991@gmail.com>
2022-01-25 18:59:35 -0800

    simple update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
-import reactDom from "react-dom";
+import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
-reactDom.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
