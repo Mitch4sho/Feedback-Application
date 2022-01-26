17a4dbf3b69cf44509780f1442569c042d43a0f0
Mitchell Oliver <Mitchelloliver1991@gmail.com>
2022-01-25 19:15:18 -0800

    fix for build to pass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { v4 as uuidv4 } from "uuid";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
-import FeedbackData from "./data/FeedBackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import AboutPage from "./pages/AboutPage";
@@ -11,19 +9,6 @@ import AboutIcon from "./components/AboutIcon";
 import { FeedbackProvider } from "./context/FeedbackContext";
 
 export default function App() {
-  const [feedback, setFeedback] = useState(FeedbackData);
-
-  const deleteFeedback = (id) => {
-    if (window.confirm("Are you sure you want to delete?")) {
-      setFeedback(feedback.filter((item) => item.id !== id));
-    }
-  };
-
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([...feedback, newFeedback]);
-  };
-
   return (
     <FeedbackProvider>
       <Router>
